891c09fcda58c32f1f8e1ee92a44ad7dece5b3bd
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2022-05-03 15:11:57 +0600

    Create dotenv and git init file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
+require('dotenv').config();
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -12,7 +13,7 @@ app.use(express.json());
 //user: dbuser1  pass:  NNzQlDlSQ7XX6YUv
 
 
-const uri = "mongodb+srv://dbuser1:NNzQlDlSQ7XX6YUv@cluster0.ujdvp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ujdvp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
